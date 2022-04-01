a5ae3659998cfb9c5fa82bb00dc14f4d0f3ba537
Kzkus <96435922+Kzkus@users.noreply.github.com>
2022-04-01 13:43:10 +0300

    Update loader.py

--- loader/loader.py
+++ loader/loader.py
@@ -7,12 +7,12 @@ logging.basicConfig(encoding='utf-8', level=logging.INFO)
 loader_blueprint = Blueprint('loader', __name__, url_prefix='/post', static_folder='static', template_folder='templates')
 
 
-@loader_blueprint.route('/form')
+@loader_blueprint.route('/form/')
 def form():
     return render_template('post_form.html')
 
 
-@loader_blueprint.route('/upload', methods=["POST"])
+@loader_blueprint.route('/upload/', methods=["POST"])
 def upload():
     try:
         file = request.files['picture']
@@ -24,7 +24,7 @@ def upload():
             'content': content
         })
         uploads_posts(posts)
-        file.save(f'uploads/images/ {filename}')
+        file.save(f'uploads/images/{filename}')
         if filename.split('.')[-1] not in ['png', 'jpeg', 'jpg']:
             logging.info('Файл не изображение')
     except FileNotFoundError:
