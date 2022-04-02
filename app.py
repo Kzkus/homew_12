780613acd7b73979e1c93067ecc173234b4964f9
Kzkus <96435922+Kzkus@users.noreply.github.com>
2022-04-02 12:27:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from loader.loader import loader_blueprint
 
 
 POST_PATH = "posts.json"
-UPLOAD_FOLDER = "/uploads/images/"
+UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
 app.register_blueprint(main_blueprint)
 app.register_blueprint(loader_blueprint)
 
 
-@app.route('/uploads/<path:path>')
+@app.route('/uploads/images/<path:path>')
 def static_dir(path):
-    return send_from_directory('/uploads/images/', path)
+    return send_from_directory('uploads/images', path)
 
 
 app.run()
